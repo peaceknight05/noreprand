9bbc0fe0c5474d0bbb9f05342f2e80a0e9fed6d0
peaceknight05 <38887622+peaceknight05@users.noreply.github.com>
2018-05-18 23:11:25 +0800

    Add files via upload
    added ruby script

---
+++ lib/noreprand.rb
@@ -0,0 +1,28 @@
+class ExceededDigitLimit < StandardError
+	def initialize(msg = "Exceeded digit limit of 10")
+		super
+	end
+end
+
+def raand(digitcount, nostartwithzero = false)
+	if digitcount > 10
+		raise ExceededDigitLimit
+	else
+		digits = Array(0..9)
+		tempnum = []
+		digitcount.times do
+			if tempnum.length == 0 && nostartwithzero == true
+				digits.delete(0)
+				randindex = rand(digits.length)
+				tempnum.push(digits[randindex])
+				digits.delete(digits[randindex])
+				digits.insert(0, 0)
+			else
+				randindex = rand(digits.length)
+				tempnum.push(digits[randindex])
+				digits.delete(digits[randindex])
+			end
+		end
+		return tempnum.join('')
+	end
+end
