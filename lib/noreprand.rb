e97c970723b8d1e76dc26df9ab131b2c27837ed8
peaceknight05 <38887622+peaceknight05@users.noreply.github.com>
2018-05-18 23:24:59 +0800

    Update noreprand.rb
    added raand into a class

--- lib/noreprand.rb
+++ lib/noreprand.rb
@@ -4,25 +4,27 @@ class ExceededDigitLimit < StandardError
 	end
 end
 
-def raand(digitcount, nostartwithzero = false)
-	if digitcount > 10
-		raise ExceededDigitLimit
-	else
-		digits = Array(0..9)
-		tempnum = []
-		digitcount.times do
-			if tempnum.length == 0 && nostartwithzero == true
-				digits.delete(0)
-				randindex = rand(digits.length)
-				tempnum.push(digits[randindex])
-				digits.delete(digits[randindex])
-				digits.insert(0, 0)
-			else
-				randindex = rand(digits.length)
-				tempnum.push(digits[randindex])
-				digits.delete(digits[randindex])
+class NoRepRand
+	def raand(digitcount, nostartwithzero = false)
+		if digitcount > 10
+			raise ExceededDigitLimit
+		else
+			digits = Array(0..9)
+			tempnum = []
+			digitcount.times do
+				if tempnum.length == 0 && nostartwithzero == true
+					digits.delete(0)
+					randindex = rand(digits.length)
+					tempnum.push(digits[randindex])
+					digits.delete(digits[randindex])
+					digits.insert(0, 0)
+				else
+					randindex = rand(digits.length)
+					tempnum.push(digits[randindex])
+					digits.delete(digits[randindex])
+				end
 			end
+			return tempnum.join('')
 		end
-		return tempnum.join('')
 	end
 end
